dfda91ca7ce86746e183187107a0de60ae0f7922
Arjun <Arjun@code.com>
2022-11-28 19:38:51 +0530

    fix: inputs value

--- isTriangle.js
+++ isTriangle.js
@@ -12,8 +12,9 @@ function checkTriangle() {
   for (let i = 0; i < inputAngle.length; i++) {
     sumofAngles+=parseInt(inputAngle[i].value)
   }
- 
-if(sumofAngles===180){
+if(inputAngle[0].value<=0 ||inputAngle[1].value<=0 ||inputAngle[2].value<=0 ){
+  showingOutput.innerHTML="Triangle cannot be zero or negative."}
+else if(sumofAngles===180){
     showingOutput.innerText="It is a Triangle";
 }else{
     showingOutput.innerHTML="Not a Triangle."
